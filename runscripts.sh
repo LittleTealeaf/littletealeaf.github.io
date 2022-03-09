833056f23eefb3afe450d22e821f49c44b6ab8b7
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-09 18:57:29 -0500

    moved findscripts.py to base directory

--- runscripts.sh
+++ runscripts.sh
@@ -3,7 +3,7 @@
 # Recursively finds all `script.py` files and executes them within their own directory
 base_dir=$(pwd)
 
-for dir in $(python3 ./scripts/findscripts.py); do
+for dir in $(python3 ./findscripts.py); do
     cd $dir
     python3 script.py
     cd $base_dir
