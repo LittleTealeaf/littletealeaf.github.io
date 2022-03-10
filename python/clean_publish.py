6cdcb62e9d84260ce3b88a15c445c8dd9c3b83b1
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-09 22:46:20 -0500

    update clean_publish.py

--- python/clean_publish.py
+++ python/clean_publish.py
@@ -1,17 +1,24 @@
 import os
+import numpy as np
 
 # Cleans the repository of any unneeded files before it finishes
 
 filterList = ['template.html']
+deleteDirs = ['./python','./templates']
 
 removeList = []
 
+def checkFile(root,filename):
+    for dir in deleteDirs:
+        if dir in root:
+            return True
+    return filename in filterList
+
 for root, dirnames, filenames in os.walk('.'):
     for filename in filenames:
-        if filename in filterList or './templates' in root:
+        if checkFile(root,filename):
             removeList.append(os.path.join(root,filename))
 
 
 for file in removeList:
     os.remove(file)
-    # print(file)
