2e755fbf7010ba96340439ff3efb3ab6f5182fd3
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-10 09:21:20 -0500

    Cleaned script

--- python/template_tools.py
+++ python/template_tools.py
@@ -1,13 +1,9 @@
 import os
 
-
-def load_template(path):
-    with open(path) as f:
-        return "".join(f.readlines())
-
 def load_templates():
     templates = {}
     for template in os.listdir('./templates/'):
         if not os.path.isdir(f'./templates/{template}'):
-            templates[template.partition('.')[0]] = load_template(f'./templates/{template}')
+            with open(f'./templates/{template}') as f:
+                templates[template.partition('.')[0]] = "".join(f.readlines())
     return templates
