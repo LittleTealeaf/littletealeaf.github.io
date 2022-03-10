62a6453a8fbf91280da52bdee63f2fc70c6ed6cb
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-03-10 03:29:32 +0000

    deploy: 13f026837ddeae4337ffb55884d4b8fff564656d

---
+++ scripts/clean_publish.py
@@ -0,0 +1,17 @@
+import os
+
+# Cleans the repository of any unneeded files before it finishes
+
+filterList = ['template.html']
+
+removeList = []
+
+for root, dirnames, filenames in os.walk('.'):
+    for filename in filenames:
+        if filename in filterList or './templates' in root:
+            removeList.append(os.path.join(root,filename))
+
+
+for file in removeList:
+    os.remove(file)
+    # print(file)
