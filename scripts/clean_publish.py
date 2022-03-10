d70b0ca593f43b0b42d657c4e40c063a84be3f20
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-09 22:10:32 -0500

    add remove section

--- scripts/clean_publish.py
+++ scripts/clean_publish.py
@@ -2,11 +2,14 @@ import os
 
 # Cleans the repository of any unneeded files before it finishes
 
+filterList =  ['template.html', 'footer.html', 'header.html']
+
 removeList = []
 
 for root, dirnames, filenames in os.walk('.'):
     for filename in filenames:
-        if filename in ['template.html', 'footer.html', 'header.html']:
+        if filename in filterList:
             removeList.append(os.path.join(root,filename))
 
-print(removeList)
+for file in removeList:
+    os.remove(file)
