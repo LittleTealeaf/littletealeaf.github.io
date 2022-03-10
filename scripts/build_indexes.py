081881b8394baaf7a29d9bdf59dc109fcb052a53
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-09 19:59:23 -0500

    formatting now works

--- scripts/build_indexes.py
+++ scripts/build_indexes.py
@@ -5,16 +5,18 @@ from template_tools import *
 # The goal of this script is to create the index in each sub-directory by formatting the templates provided
 
 
-header = load_template('./templates/header.html')
-footer = load_template('./templates/footer.html')
+# Build templates into dictionary
+templates = {}
+for template in os.listdir('./templates/'):
+    templates[template.partition('.')[0]] = load_template(f'./templates/{template}')
+
 
 # For each directory with a template.html
 for dir in map(lambda dirinfo: dirinfo[0], filter(lambda dirinfo: 'template.html' in dirinfo[2], os.walk('.'))):
+    
+    # Build templates
     print(f'Building {dir}')
     template = load_template(os.path.join(dir,'template.html'))
     with open(os.path.join(dir,'index.html'),'w') as file:
-        file.write(template.format(
-            header = header,
-            footer = footer
-        ))
+        file.write(template.format(**templates))
     
