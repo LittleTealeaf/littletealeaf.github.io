64293dd4b2f608ad58646f8e5d7276650058e8c7
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-10 16:39:17 -0500

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+LOG="log.txt"
+
+
+echo "" > $LOG
+
 echo "Installing Python Requirements"
-python3 -m pip install -r ./python/requirements.txt >> "log.txt"
+python3 -m pip install -r ./python/requirements.txt >> $LOG
 echo "Python Requirements Installed"
 
-
+echo "Cleaning build directory"
+rm -r ./build >> $LOG
+mkdir build/ >> $LOG
+echo "New Build Directory Created"
