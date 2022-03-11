ee4d5be4b7bd27bb5ed80fbead0199e30868a52f
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-11 14:33:44 -0500

    fixed rename in util_resources.py

--- python/util_resources.py
+++ python/util_resources.py
@@ -5,5 +5,5 @@ def res_gen_name(seed,extension,length=10):
     random.seed(str(seed))
     letters = ['abcdefghijklmnopqrstuvwxyz1234567890']
     name = "".join([letters[random.randint(0,len(letters))] for i in range(length)]) 
-    return f'./resources/generated/{name}.{extension}'
+    return f'./resources/gen/{name}.{extension}'
 
