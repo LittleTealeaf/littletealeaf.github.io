4d187c5019afda7dbfefdaa91d6a5ad34aea0827
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-11 14:11:06 -0500

    Use BSFishy/pip-action to install dependencies

--- build.sh
+++ build.sh
@@ -5,10 +5,6 @@ LOG="log.txt"
 
 echo "" > $LOG
 
-echo "Installing Python Requirements"
-python3 -m pip install -r ./python/requirements.txt >> $LOG
-echo "Python Requirements Installed"
-
 echo "Cleaning build directory"
 rm -r ./build >> $LOG
 mkdir build/ >> $LOG
