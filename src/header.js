ec27fdb88d16fafa2430c668a901eb8dc486fc72
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-12 18:03:01 -0500

    created website

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import './header.css'
+
+function Header() {
+    return (
+        <div class="header">
+            <div class="title">
+                <h1>Thomas Kwashnak</h1>
+                <h2>LittleTealeaf</h2>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header;
