24bc6dc46d68f6f6785a8a0cfef1d024bd5d45b8
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-12 19:32:14 -0500

    Changed functions to variables

--- python/res.py
+++ python/res.py
@@ -1,14 +1,12 @@
 import os, json, shutil
 
-def dir_gen_src():
-    return os.path.join('.','src','gen')
+dir_gen_src = os.path.join('.','src','gen')
+dir_gen_public = os.path.join('.','public','gen')
 
-def dir_gen_public():
-    return os.path.join('.','public','gen')
 
 def delete_directory_if_exists(dir):
     if os.path.exists(dir) and os.path.isdir(dir):
         shutil.rmtree(dir)
 
-delete_directory_if_exists(dir_gen_public())
-delete_directory_if_exists(dir_gen_src())
+delete_directory_if_exists(dir_gen_public)
+delete_directory_if_exists(dir_gen_src)
