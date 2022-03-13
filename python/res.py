cfbfcab35a68bb30594907f8ecfd67468dc53a02
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-12 19:29:36 -0500

    deletes directory if exists

--- python/res.py
+++ python/res.py
@@ -1,9 +1,14 @@
-import os, json
+import os, json, shutil
 
 def dir_gen_src():
-    return os.join('.','src','gen')
+    return os.path.join('.','src','gen')
 
 def dir_gen_public():
-    return os.join('.','public','gen')
+    return os.path.join('.','public','gen')
 
-    
+def delete_directory_if_exists(dir):
+    if os.path.exists(dir) and os.path.isdir(dir):
+        shutil.rmtree(dir)
+
+delete_directory_if_exists(dir_gen_public())
+delete_directory_if_exists(dir_gen_src())
