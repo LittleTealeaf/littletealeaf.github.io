653fe44c93c5c1f56313aff1228cf092453a2a4e
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-12 20:05:41 -0500

    res.py now generates directories

--- python/res.py
+++ python/res.py
@@ -4,13 +4,15 @@ dir_gen_src = os.path.join('.','src','gen')
 dir_gen_public = os.path.join('.','public','gen')
 
 
-def delete_directory_if_exists(dir):
+def initialize_generated_dir(dir):
     if os.path.exists(dir) and os.path.isdir(dir):
-        print("Deleted Directory: " + dir)
+        print(f"Deleting {dir}")
         shutil.rmtree(dir)
+    print(f"Initializing {dir}")
+    os.makedirs(dir)
 
-delete_directory_if_exists(dir_gen_public)
-delete_directory_if_exists(dir_gen_src)
+initialize_generated_dir(dir_gen_public)
+initialize_generated_dir(dir_gen_src)
 
 def resource_src(name):
     "returns the path for a file with the provided name in the src generated directory"
