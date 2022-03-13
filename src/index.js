363f89076d8733483d5ef68431cb3a5970dc292d
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-13 11:19:01 -0400

    renamed header dir to template dir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './header/header'
+import Header from './templates/header'
 
 ReactDOM.render(
   <React.StrictMode>
