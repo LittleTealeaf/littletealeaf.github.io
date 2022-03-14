aa460dc0ecac0a820ce318f26bf5e2f1dedc1acf
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-14 19:42:29 -0400

    removed unused import

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 
-import App from './App'
-import Header from './components/header';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
