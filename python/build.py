aae5fbc39c70e3219e06ce7532b55a26ce80ec9f
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-14 16:23:21 -0400

    optimized json compiling

--- python/build.py
+++ python/build.py
@@ -4,19 +4,13 @@ from githubapi import *
 
 # Compiles the projects json for building
 with open(os.path.join('.','resources','projects.json')) as f:
-    projects_in = json.load(f)
-    projects = []
+    projects = json.load(f)
 
-    for p in projects_in:
-        project = {'api':api_github(p['api_url'])}
+    for project in projects:
+        project['api'] = api_github(project['api_url'])
         project['contributors'] = api_github(project['api']['contributors_url'])
         project['tags'] = api_github(project['api']['tags_url'])
         project['releases'] = api_github(project['api']['releases_url'].replace('{/id}',''))
-        
-        
-
-
-        projects.append(project)
     
     with open(resource_src('projects.json'),'w') as w:
         w.write(json.dumps(projects))
