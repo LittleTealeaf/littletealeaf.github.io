5a2d2f47865336419a252e252567c30b7562175e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-14 19:32:11 -0400

    so.. multiple pages maybe?

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './components/header';
+import Home from './components/home';
+
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+             <Routes>
+                    <Route path="/" component={Home} exact/>
+                </Routes>   
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './templates/header';
 import './styles/index.css';
+
+import App from './App'
+import Header from './components/header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
+    < Header />
+  </React.StrictMode>,
+  document.getElementById('header')
+)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
