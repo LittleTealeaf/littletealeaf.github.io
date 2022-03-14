d35d15a59067845547c53e70f3a1bef0b4005fa8
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-14 19:33:13 -0400

    put home in new pages directory

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './components/header';
-import Home from './components/home';
+import Home from './pages/home';
 
 
 class App extends Component {
