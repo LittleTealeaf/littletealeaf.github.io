a77a53ad21dc981d02d7300b4072b8e5646ac017
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-15 12:21:50 -0400

    resources in public

--- python/build.py
+++ python/build.py
@@ -11,6 +11,14 @@ with open(os.path.join('.','resources','projects.json')) as f:
         project['api'] = api_github(project['api_url'])
         def compile_user(api):
             c = {'api':api}
+            img = image_format(image_src(api['avatar_url']),{
+                'circular': True
+            })
+            img_resource = Resource(PUBLIC,seed=f'{api["avatar_url"]}imageround',suffix='.png')
+            
+            img.save(img_resource.path)
+            c['avatar_url'] = img_resource.refpath
+
             # img = image_format(image_src(api['avatar_url']),{
             #     'circular': True
             # })
--- python/resutil.py
+++ python/resutil.py
@@ -30,31 +30,3 @@ RESOURCES = ResourceDirectory(os.path.join('.','resources','generated'))
 PUBLIC = ResourceDirectory(os.path.join('.','public','resources','generated'),'/resources/generated')
 
 [i.initialize() for i in [RESOURCES,PUBLIC]]
-
-print(RESOURCES.directory)
-
-# def resource(location,name='',suffix='',randomName=False,seed=None):
-#     if seed:
-#         random.seed(str(seed))
-#     if randomName:
-#         name = "".join(random.sample('abcdefghijklmnopqrstuvwxyz1234567890',10))
-#     return (os.path.join(*location[0],f'{name}{suffix}'),"/".join([*location[1],f'{name}{suffix}']))
-
-
-
-# PUBLIC = (['.','public','res','gen'],['%PUBLIC_URL%','res','gen'])
-# "The first entry of this tuple contains the path from the project root, the second entry contains the path that source files should use"
-# SRC = (['.','pages','res','gen'],['.','pages','gen'])
-# "The first entry of this tuple contains the path from the project root, the second entry contains the path that source files should use"
-
-# def initialize_dirs(*dirs):
-#     for dir in dirs:
-#         path = os.path.join(*dir[0])
-#         if os.path.exists(path) and os.path.isdir(path):
-#             print(f"Deleting {path}")
-#             shutil.rmtree(path)
-#         print(f"Initializing {path}")
-#         os.makedirs(path)
- 
-# initialize_dirs(PUBLIC,SRC)
-
