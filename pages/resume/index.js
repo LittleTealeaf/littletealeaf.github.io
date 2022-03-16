49d72cdbff919c38cefa37fd3aed149c0171c6ee
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-16 10:31:45 -0400

    added resume page

---
+++ pages/resume/index.js
@@ -0,0 +1,12 @@
+import Header from '../../components/header'
+
+export default function Home() {
+    return (
+      <div>
+        <Header path={
+          ["Resume"]
+        }/>
+        I am a resume bird
+      </div>
+    )
+  }
