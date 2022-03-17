f2ac490a094f55340f62bc76abdc086b1d4937e7
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-16 21:02:27 -0400

    added 404

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import Header from '../components/header'
+import Style from '../styles/404.module.css'
+
+export default function Home() {
+    return (
+
+        <div>
+            <Header path={[]} />
+            <div>
+
+                <h1 className={Style.leftText}>404</h1>
+                <div className={Style.verticalBar}>
+                    <h2 className={Style.rightText}>This page could not be found.</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
