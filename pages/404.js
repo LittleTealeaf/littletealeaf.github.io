bcc85a5b01d2b41faa0c82b25094134b63055f53
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-16 21:06:12 -0400

    Added 404 page not found

--- pages/404.js
+++ pages/404.js
@@ -8,10 +8,13 @@ export default function Home() {
             <Header path={[]} />
             <div>
 
+                <center className={Style.center}>
                 <h1 className={Style.leftText}>404</h1>
-                <div className={Style.verticalBar}>
-                    <h2 className={Style.rightText}>This page could not be found.</h2>
-                </div>
+
+                    <div className={Style.verticalBar}>
+                        <h2 className={Style.rightText}>This page could not be found.</h2>
+                    </div>
+                    </center>
             </div>
         </div>
     )
