eb2c819e8fcb1f977ac584f2bd824a218ae3336e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-17 18:23:32 -0400

    python rewriteeee

--- python/build.py
+++ python/build.py
@@ -1,131 +1,21 @@
-import os, json, requests
-from sys import stdin
 from assetutil import *
-from githubapi import *
+from githubutil import *
+from jsonutil import *
 from imageutil import *
+from config import *
 
-# Run Initialization Scripts
 clean_directory()
-
-
-settings = {}
-"Settings loaded from the pyconfig.json file in assets"
-with open(os.path.join('.','assets','pyconfig.json')) as f:
-    settings = json.load(f)
 
 index = {}
 
-def save_json(asset,dict):
-    "Directly saves a json to an asset object"
-    with open(asset.path,'w') as w:
-        w.write(json.dumps(dict))
+index['user'] = ref_github_user(username='LittleTealeaf',followers=True,following=True)
 
-def load_json(asset):
-    data = {}
-    with open(asset.path) as f:
-        data = json.load(f)
-    return data
-
-def reference_json(asset,json):
-    "Directly saves a json to an asset object and returns the reference"
-    save_json(asset,json)
-    return asset.ref
-
-def fetch_json(asset,fetch):
-    "Checks if that asset exists, calls `fetch` with `params` if it does not exist. Returns the reference"
-    if not asset.exists():
-        print(f'Fetching Asset: {asset.ref}')
-        with open(asset.path,'w') as w:
-            w.write(json.dumps(fetch()))
-    return asset.ref
-
-def reference_image(img):
-    "Saves an image based on the hash of it's contents, then returns the reference"
-    asset = Asset(IMAGES,seed=image_hash(img),type=PNG)
-    img.save(asset.path)
-    return asset.ref
-        
-def reference_github_user(username=None,url=None,api=None, include=[], update=False):
-    if not url:
-        if username:
-            url = f'https://api.github.com/users/{username}'
-        elif api:
-            url = api['url']
-
-    asset = Asset(path=GITHUB_USER,seed=url,type=JSON)
-
-    def fetchUser(user=None,url=None, include=[]):
-        if not user:
-            user = api_github(url)
-        user.update({
-            'avatar': reference_image(image_format(image_src(user['avatar_url']),{'circular': True}))
-        })
-
-        for group in ['followers', 'following']:
-            if group in include:
-                user[group] = reference_json_seed(GITHUB_USER_LIST,[reference_github_user(api=user) for user in api_github(user[f'{group}_url'].partition('{')[0])])
-
-        [user.pop(item,None) for item in ['private_gists','plan','total_private_repos','owned_private_repos','two_factor_authentication']]
-
-        return user
-
-
-
-    if update and os.path.exists(asset.path):
-        loaded = load_json(asset)
-        loaded.update(fetchUser(user=api,url=url, incldue=include))
-        save_json(asset,loaded)
-    else:
-        return fetch_json(asset,lambda: fetchUser(api,url,include))
-
-def reference_api_github(url,path=GITHUB):
-    asset = Asset(path=path,seed=url,type=JSON)
-    return fetch_json(asset,lambda: api_github(url))
-
-def reference_json_seed(path=[],data=None):
-    json_string = json.dumps(data)
-    return reference_json(Asset(path=path,seed=json_string,type=JSON),data)
-
-def reference_github_repository(url):
-    asset = Asset(GITHUB_REPOSITORY,seed=url,type=JSON)
-    if not asset.exists():
-        repo = api_github(url)
-        repo.update({
-            'contributors': reference_json_seed(GITHUB_USER_LIST,[{'user':reference_github_user(api=user_api),'contributions':user_api['contributions']} for user_api in api_github(repo['contributors_url'])]),
-            'subscribers': reference_json_seed(GITHUB_USER_LIST,[reference_github_user(api=user_api) for user_api in api_github(repo['subscribers_url'])]),
-            'stargazers': reference_json_seed(GITHUB_USER_LIST,[reference_github_user(api=user_api) for user_api in api_github(repo['stargazers_url'])]),
-            'languages': reference_json(Asset(GITHUB_LANGUAGES,type=JSON,seed=repo['languages_url']),api_github(repo['languages_url'])),
-            'events': reference_api_github(repo['events_url'],GITHUB_EVENTS),
-            'releases': reference_api_github(repo['releases_url'].replace('{/id}',''),GITHUB_RELEASES),
-            'owner': reference_github_user(api=repo['owner'])
-        })
-        [repo.pop(key,None) for key in ['permissions','contributors_url','subscribers_url','stargazers_url','languages','events_url','releases_url']]
-        save_json(asset,repo)
-    return asset.ref
-
-
-
-# Actual build script
-index['user'] = reference_github_user(username=settings['username'],include=['followers','following'])
-
-
-
-
-with open(os.path.join('.','assets','projects.json')) as p:
-    projects = []
-
-    for project_ref in json.load(p):
-        project = {
-            'repository': reference_github_repository(project_ref['repository_api'])
-        }
-
-
-        if 'attributes' in project_ref:
-            project['attributes'] = reference_json_seed(JSONS,project_ref['attributes'])
-        
-        projects.append(reference_json_seed(JSONS,project))
+with open(os.path.join('.','assets','projects.json')) as file:
+    projects = json.load(file)
+    for project in projects:
+        project['repository'] = ref_github_repository(project['repository'])
+        project['attributes'] = ref_json(project['attributes'])
     
-    index['projects'] = reference_json_seed(JSONS,projects);
+    index['projects'] = ref_json([ref_json(i) for i in projects])
 
-
-save_json(Asset(name='index.json'),index)
+save_json(index,Asset(name='index.json'))
--- python/imageutil.py
+++ python/imageutil.py
@@ -1,41 +1,28 @@
 import os, glob, numpy as np, requests, random
 from PIL import Image, ImageDraw, ImageFilter
 from io import BytesIO
+from assetutil import *
 
-def image_format_circular(img):
-    h,w = img.size
+def ref_image(url,asset_path=IMAGES,type=PNG,circular=False):
+    img = Image.open(BytesIO(requests.get(url).content)).convert('RGBA')
 
-    alpha = Image.new('L',img.size,0)
-    draw = ImageDraw.Draw(alpha)
-    draw.ellipse((0,0,h,w),fill=255)
+    if circular:
+        h,w = img.size
 
-    img.putalpha(alpha.filter(ImageFilter.GaussianBlur(4)))
+        alpha = Image.new('L',img.size,0)
+        draw = ImageDraw.Draw(alpha)
+        draw.ellipse((0,0,h,w),fill=255)
 
-    return img
-
-def image_read(path):
-    print(f'Loading image from {path}')
-    return Image.open(path)
-
-def image_src(url):
-    print(f'Loading image from {url}')
-    return Image.open(BytesIO(requests.get(url).content))
-
-def image_format(image,attributes={}):
-    img = image.convert('RGBA')
-    if 'circular' in attributes and attributes['circular']:
-        img = image_format_circular(img)
+        img.putalpha(alpha.filter(ImageFilter.GaussianBlur(4)))
     
-    size = img.size
-    if 'width' in attributes:
-        size[0] = attributes['width']
-    if 'height' in attributes:
-        size[1] = attributes['height']
-    img = img.resize(size)
-    return img
+    
+    asset = Asset(path=asset_path,type=type,seed=hash_image(img))
+    img.save(asset.path)
 
-# comment out later
-def image_hash(img):
+    return asset.ref
+
+
+def hash_image(img):
     image = img.resize((10,10),Image.ANTIALIAS)
     image = image.convert("L")
     pixel_data = list(image.getdata())
@@ -43,3 +30,36 @@ def image_hash(img):
     bits = "".join(['1' if (px >= avg_pixel) else '0' for px in pixel_data])
     hex_representation = str(hex(int(bits, 2)))[2:][::-1].upper()
     return hex_representation
+
+
+# def image_format_circular(img):
+#     h,w = img.size
+
+#     alpha = Image.new('L',img.size,0)
+#     draw = ImageDraw.Draw(alpha)
+#     draw.ellipse((0,0,h,w),fill=255)
+
+#     img.putalpha(alpha.filter(ImageFilter.GaussianBlur(4)))
+
+#     return img
+
+# def image_read(path):
+#     print(f'Loading image from {path}')
+#     return Image.open(path)
+
+# def image_src(url):
+#     print(f'Loading image from {url}')
+#     return Image.open(BytesIO(requests.get(url).content))
+
+# def image_format(image,attributes={}):
+#     img = image.convert('RGBA')
+#     if 'circular' in attributes and attributes['circular']:
+#         img = image_format_circular(img)
+    
+#     size = img.size
+#     if 'width' in attributes:
+#         size[0] = attributes['width']
+#     if 'height' in attributes:
+#         size[1] = attributes['height']
+#     img = img.resize(size)
+#     return img
