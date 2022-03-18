cadb315a1af65c949d60388ad01798ddf48007df
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-18 12:31:14 -0400

    removed additional unused lines

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,13 +1,13 @@
 import Header from '../../components/header'
 import StyleClass from '../../libs/styleutil'
 import style from '../../styles/style.module.css'
-import { getAsset, Resume} from '../../libs/assets'
+import { getAsset, Resume } from '../../libs/assets'
 
 const Skills = () => {
 
   const SkillCategory = (category, i) => {
 
-    const SkillsList  = (skill) => {
+    const SkillsList = (skill) => {
       if (skill.attributes.length > 0) {
         return `${skill.name} (${skill.attributes.join(', ')})`;
       } else {
@@ -21,27 +21,24 @@ const Skills = () => {
       </div>
     )
   }
-
-
-  const skills = getAsset(Resume.skills);
   return (
     <div className={StyleClass(style.section)}>
       <h1>Skills</h1>
-      {skills.map(SkillCategory)}
+      {getAsset(Resume.skills).map(SkillCategory)}
     </div>
   )
 };
 
 export default function Home() {
-    return (
-      <div>
-        <Header path={
-          ["Resume"]
-        }/>
-        <center>
-          <h1 className={StyleClass(style.section, style.header1)}>{Resume.name}</h1>
-          {Skills()}
-        </center>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Header path={
+        ["Resume"]
+      } />
+      <center>
+        <h1 className={StyleClass(style.section, style.header1)}>{Resume.name}</h1>
+        {Skills()}
+      </center>
+    </div>
+  )
+}
