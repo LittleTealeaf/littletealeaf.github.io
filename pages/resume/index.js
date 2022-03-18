71fc123a4aee52227b143b135b317af343f2e0b6
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-18 13:46:27 -0400

    formatting skills

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -8,23 +8,39 @@ const Skills = () => {
   const SkillCategory = (category, i) => {
 
     const SkillsList = (skill) => {
-      if (skill.attributes.length > 0) {
-        return `${skill.name} (${skill.attributes.join(', ')})`;
-      } else {
-        return skill.name;
-      }
+
+      const item = skill.attributes.length > 0 ? <abbr title={skill.attributes.join(', ')}>{skill.name}</abbr> : <div>{skill.name}</div>;
+
+      return <div style={{'flex-grow':1,'textAlign':'left'}}>
+        {item}
+      </div>
     }
 
     return (
-      <div id={i}>
-        <p><b>{category.name}</b> {getAsset(category.values).map(SkillsList).join(', ')}</p>
+      <div id={i} style={{
+        'flex-grow': 1,
+        'padding':'5px'
+        }}>
+        {/* <div><b>{category.name}</b> {getAsset(category.values).map(SkillsList)}</div> */}
+        <div style={{
+          'textAlign':'left'
+        }}><b>{category.name}</b></div>
+        <div style={{
+          'display':'flex',
+          'flex-direction':'column', 
+          'flex-wrap':'wrap'
+          }}>
+        {getAsset(category.values).map(SkillsList)}
+        </div>
       </div>
     )
   }
   return (
     <div className={StyleClass(style.section)}>
       <h1>Skills</h1>
-      {getAsset(Resume.skills).map(SkillCategory)}
+      <div style={{'display':'flex', 'flex-direction':'row'}}>
+        {getAsset(Resume.skills).map(SkillCategory)}
+      </div>
     </div>
   )
 };
