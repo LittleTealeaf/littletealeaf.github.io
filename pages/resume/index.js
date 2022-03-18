dde8dbe54c0c86b3910516c8ef8aada0f826c4ba
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-18 15:33:38 -0400

    reformatted skils

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -4,55 +4,40 @@ import style from '../../styles/style.module.css'
 import { getAsset, Resume } from '../../libs/assets'
 
 const Skills = () => {
+  const SkillCategory = (cateogry, i) => {
 
-  const SkillCategory = (category, i) => {
-
-    const SkillsList = (skill) => {
-
-      const item = skill.attributes.length > 0 ? <abbr title={skill.attributes.join(', ')}>{skill.name}</abbr> : <div>{skill.name}</div>;
-
-      return <div style={{'flex-grow':1,'textAlign':'left'}}>
-        {item}
-      </div>
-    }
+    const SkillDisplay = (skill) => (
+      skill.attributes.length == 0 ? skill.name : `${skill.name} (${skill.attributes.join(', ')})`
+    );
 
     return (
-      <div id={i} style={{
-        'flex-grow': 1,
-        'padding':'5px'
-        }}>
-        {/* <div><b>{category.name}</b> {getAsset(category.values).map(SkillsList)}</div> */}
-        <div style={{
-          'textAlign':'left'
-        }}><b>{category.name}</b></div>
-        <div style={{
-          'display':'flex',
-          'flex-direction':'column', 
-          'flex-wrap':'wrap'
-          }}>
-        {getAsset(category.values).map(SkillsList)}
-        </div>
-      </div>
-    )
+      <tr key={i}>
+        <td><b>{cateogry.name}</b></td>
+        <td>{getAsset(cateogry.values).map(SkillDisplay).join(", ")}</td>
+      </tr>
+    );
   }
+
   return (
     <div className={StyleClass(style.section)}>
-      <h1>Skills</h1>
-      <div style={{'display':'flex', 'flex-direction':'row'}}>
+      <h1 className={style.header1} style={{
+        'margin': '0px'
+      }}>Skills</h1>
+      <table>
         {getAsset(Resume.skills).map(SkillCategory)}
-      </div>
+      </table>
     </div>
-  )
+  );
 };
 
-export default function Home({router}) {
-    return (
-      <div>
-        <Header router={router}/>
-        <center>
-          <h1 className={StyleClass(style.section, style.header1)}>{Resume.name}</h1>
-          {Skills()}
-        </center>
-      </div>
-    )
-  }
+export default function Home({ router }) {
+  return (
+    <div>
+      <Header router={router} />
+      <center>
+        <h1 className={StyleClass(style.section, style.header1)}>{Resume.name}</h1>
+        {Skills()}
+      </center>
+    </div>
+  )
+}
