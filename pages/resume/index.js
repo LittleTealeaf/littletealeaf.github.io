74e24b0083a9cb2fbc952970174735131de208c7
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-17 22:42:45 -0400

    need to do something in terms of how the resume is setup..

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,4 +1,10 @@
 import Header from '../../components/header'
+import StyleClass from '../../libs/styleutil'
+import style from '../../styles/style.module.css'
+import { Resume} from '../../libs/assets'
+
+
+
 
 export default function Home() {
     return (
@@ -6,7 +12,9 @@ export default function Home() {
         <Header path={
           ["Resume"]
         }/>
-        I am a resume bird
+        <center>
+          <h1 className={StyleClass(style.section, style.header1)}>{Resume.name}</h1>
+        </center>
       </div>
     )
   }
