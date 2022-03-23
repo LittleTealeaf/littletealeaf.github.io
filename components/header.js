f8f323995ab997f60f9ef26d5938cae8db12ab97
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-22 20:43:08 -0400

    Added example header

--- components/header.js
+++ components/header.js
@@ -35,8 +35,50 @@ export default function Header({ router, title: page_title }) {
                 </div>
             </div>
         </div>
+        
     )
 }
+/*
+https://www.section.io/engineering-education/creating-a-responsive-navigation-bar-using-tailwind-css-and-javascript/
+<nav class="bg-white shadow-lg">
+		<div class="max-w-6xl mx-auto px-4">
+			<div class="flex justify-between">
+				<div class="flex space-x-7">
+					<div>
+						<a href="#" class="flex items-center py-4 px-2">
+							<img src="logo.png" alt="Logo" class="h-8 w-8 mr-2" />
+							<span class="font-semibold text-gray-500 text-lg"
+								>Navigation</span
+							>
+						</a>
+					</div>
+					<div class="hidden md:flex items-center space-x-1">
+						<a
+							href=""
+							class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
+							>Home</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+							>Services</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+							>About</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+							>Contact Us</a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+    */
 
 /*
 <div className={style.title}>
