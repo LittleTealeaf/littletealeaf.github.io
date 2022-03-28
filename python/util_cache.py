dda26a0b140fe624e42f6b1c34c238737af10eb2
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-28 13:51:33 -0400

    put cache in its own directory

--- python/util_cache.py
+++ python/util_cache.py
@@ -5,16 +5,16 @@ import time
 import random
 import util_assets as assets
 
+CACHE_DIRECTORY = os.path.join('.','cache','python')
+if not os.path.exists(CACHE_DIRECTORY):
+    os.makedirs(CACHE_DIRECTORY)
 
-
-def save(key: str, value: any, expire_min: int = 0, expire_max: int = 24):
+def save(key: str, value: any, expire: int = 24):
     file_name: str = hash_key(key)
-    path = os.path.join('.','cache',f'{file_name}.json')
+    path = os.path.join(CACHE_DIRECTORY,f'{file_name}.json')
 
-    expires = current_time() + random.randint(expire_min,expire_max) * 1000 * 60 * 60
+    expires = current_time() + expire * 1000 * 60 * 60
 
-    if not os.path.exists(os.path.join('.','cache')):
-        os.mkdir(os.path.join('.','cache'))
     with open(path,'w') as file:
         file.write(json.dumps({
             'key': key,
@@ -26,7 +26,7 @@ def save(key: str, value: any, expire_min: int = 0, expire_max: int = 24):
 
 def load(key: str):
     file_name: str = hash_key(key)
-    path = os.path.join('.','cache',f'{file_name}.json')
+    path = os.path.join(CACHE_DIRECTORY,f'{file_name}.json')
     
     if os.path.exists(path):
         data = None
