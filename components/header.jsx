80bcf2998aec482b302705d6e7cdb99c9f947799
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-03 17:01:21 -0400

    Flex stuff with tailwind

--- components/header.jsx
+++ components/header.jsx
@@ -13,21 +13,19 @@ export default function Header() {
         ))}
       </Head>
       <img src={require('../assets/images/header.jpg')} alt="header image" style={{
-        width: '100%'
+        width: '100%',
       }}/>
       <ul
-        className="sticky top-0"
+        className="sticky top-0 bg-black list-none text-white flex"
         style={{
           height: HEADER_HEIGHT,
           width: "100%",
-          background: 'black',
-          listStyleType: 'none',
-          color: 'white'
+          fontSize: '28px'
         }}
       >
         {require("../assets/navigation.json").map((item,index) => (
-          <li key={index} style={{
-            float: 'left'
+          <li key={index} className="float-left bg-black hover:bg-gray-700 flex-grow grow-1 hover:grow-[2]" style={{
+            padding: '14px 16px'
           }}>
             <Link href={item.href}>
             <p className="cursor-pointer">{item.name}</p>
