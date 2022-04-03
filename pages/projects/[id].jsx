92db6190cf371674408cd3aae07dd1b6c0d22040
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-03 18:43:53 -0400

    Fixed inability to compile

--- pages/projects/[id].jsx
+++ pages/projects/[id].jsx
@@ -1,18 +1,27 @@
 
 
 export default function Page({}) {
-
+    return (
+        <>
+        <div>
+            well this is awkward
+        </div>
+        </>
+    )
 }
 
 
 export async function getStaticPaths() {
-    
+    return {
+        paths: [],
+        fallback: false
+    }
 }
 
 export async function getStaticProps({ params }) {
     return {
         props: {
-            
+            id: params.id
         }
     }
 }
