4e9b77577b7b4f4af973ca9e89be7ae0a5f1d95d
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-03 18:54:16 -0400

    Projects get their own page

--- pages/projects/[id].jsx
+++ pages/projects/[id].jsx
@@ -1,10 +1,18 @@
+import { getGenerated } from "../../libs/resources"
+import Head from 'next/head'
 
 
-export default function Page({}) {
+export default function Page({id, generatedRef}) {
+    const project = getGenerated(generatedRef);
     return (
         <>
+        <Head>
+            <title>
+                {project.name}
+            </title>
+        </Head>
         <div>
-            well this is awkward
+            {project.name}
         </div>
         </>
     )
@@ -12,16 +20,24 @@ export default function Page({}) {
 
 
 export async function getStaticPaths() {
+    const projects = getGenerated('index.json').pages.projects;
     return {
-        paths: [],
+        paths: Object.keys(projects).map((key) => ({
+            params: {
+                id: key
+            }
+        })),
         fallback: false
     }
 }
 
 export async function getStaticProps({ params }) {
+    const genRef = getGenerated('index.json').pages.projects[params.id];
+
     return {
         props: {
-            id: params.id
+            id: params.id,
+            genRef
         }
     }
 }
