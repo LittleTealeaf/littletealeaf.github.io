89493ca7a113b681eeaaf8c6b46501d686738f74
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-03 17:11:14 -0400

    General Header Formatting Done

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,9 @@
 import Head from "next/head";
-import Link from 'next/link'
+import Link from "next/link";
 
-const HEADER_HEIGHT = 70;
+const HEADER_HEIGHT = 60;
 
 export default function Header() {
-
   return (
     <>
       <Head>
@@ -12,27 +11,34 @@ export default function Header() {
           <meta key={index} name={item.name} content={item.content} />
         ))}
       </Head>
-      <img src={require('../assets/images/header.jpg')} alt="header image" style={{
-        width: '100%',
-      }}/>
+      <img
+        src={require("../assets/images/header.jpg")}
+        alt="header image"
+        style={{
+          width: "100%",
+        }}
+      />
       <ul
         className="sticky top-0 bg-black list-none text-white flex"
         style={{
           height: HEADER_HEIGHT,
           width: "100%",
-          fontSize: '28px'
+          fontSize: "28px",
         }}
       >
-        {require("../assets/navigation.json").map((item,index) => (
-          <li key={index} className="float-left bg-black hover:bg-gray-700 flex-grow grow-1 hover:grow-[2]" style={{
-            padding: '14px 16px'
-          }}>
+        {require("../assets/navigation.json").map((item, index) => (
+          <li
+            key={index}
+            className="float-left transition-all bg-black hover:bg-gray-700 grow-[3] hover:grow-[4] text-center"
+            style={{
+              padding: "14px 16px",
+            }}
+          >
             <Link href={item.href}>
-            <p className="cursor-pointer">{item.name}</p>
+              <p className="cursor-pointer">{item.name}</p>
             </Link>
           </li>
         ))}
-
       </ul>
       <div
         style={{
