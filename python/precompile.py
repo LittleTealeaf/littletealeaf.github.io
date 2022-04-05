dc3697fc47050e8ecf59e57249d19a303aa433ee
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-05 14:44:40 -0400

    Removed unused generators

--- python/precompile.py
+++ python/precompile.py
@@ -3,6 +3,3 @@ import libs.generated as gen
 import json
 
 gen.initialize()
-
-gen.refjson(Github.getAPIList('https://api.github.com/repos/LittleTealeaf/littletealeaf.github.io/commits'),'api','test','littletealeaf.github.io.json')
-gen.refjson(Github.getAPIList('https://api.github.com/repos/LittleTealeaf/paceManager/commits'),'api','test','paceManager.json')
