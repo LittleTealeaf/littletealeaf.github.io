dbe1688d0b4575d25fd564e83d55c071f0d89032
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-05 15:01:24 -0400

    Import api and languages for projects

--- python/precompile.py
+++ python/precompile.py
@@ -7,10 +7,17 @@ gen.initialize()
 
 index = {}
 
-index['projects'] = conf.getFiles('projects')
-index['blogs'] = conf.getFiles('blogs')
+# index['projects'] = conf.getFiles('projects')
+# index['blogs'] = conf.getFiles('blogs')
 
-# index['projects'] = []
+index['projects'] = []
+for project_path in conf.getFiles('projects'):
+    project = conf.getJSON(*project_path)
+
+    project['github']['api'] = Github.getAPI(f"https://api.github.com/repos/{project['github']['repo']}")
+    project['github']['languages'] = Github.getAPI(project['github']['api']['languages_url'])
+
+    index['projects'].append(gen.refjson(project,'pages','projects',project_path[-1]))
 
 
 gen.refjson(index,'index.json')
