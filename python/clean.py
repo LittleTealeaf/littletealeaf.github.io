3e7be2d6af1c0930deff5a4dd6e1ead32e945905
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-05 17:20:32 -0400

    Fixed integer conversion

--- python/clean.py
+++ python/clean.py
@@ -3,7 +3,7 @@
 import os
 import shutil
 
-if 'RUN_NUMBER' not in os.environ or os.environ['RUN_NUMBER']%100==0:
+if 'RUN_NUMBER' not in os.environ or int(os.environ['RUN_NUMBER'])%100==0:
     print("Cleaning Caches")
     shutil.rmtree(os.path.join('.','cache'))
     os.mkdir(os.path.join('.','cache'))
