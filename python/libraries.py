159c07e58d203b76e292c218e9190e7848e732f0
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-05 13:28:00 -0400

    Implemented API

--- python/libraries.py
+++ python/libraries.py
@@ -1,28 +1,29 @@
 import os
 import time
 import json
+from tokenize import Number
+import requests
 
 
 class Cache:
-    def __init__(self,*path):
-        self.path = os.path.join('.','cache',*path) + '.json'
+    def __init__(self, *path):
+        self.path = os.path.join('.', 'cache', *path) + '.json'
+        for key in self.load():
+            self.get(key)
 
-    @classmethod
     def load(self):
         cache = {}
         if os.path.exists(self.path):
             with open(self.path) as file:
                 cache = json.load(file)
         return cache
-    
-    @classmethod
-    def save(self,cache):
-        os.makedirs(os.path.dirname(self.path),exist_ok=True)
-        with open(self.path,'w') as file:
+
+    def save(self, cache):
+        os.makedirs(os.path.dirname(self.path), exist_ok=True)
+        with open(self.path, 'w') as file:
             file.write(json.dumps(cache))
-    
-    @classmethod
-    def get(self,key):
+
+    def get(self, key):
         cache = self.load()
         if key in cache:
             if cache[key]['expires'] > self.get_time():
@@ -31,9 +32,8 @@ class Cache:
                 cache.pop(key)
                 self.save(cache)
         return None
-    
-    @classmethod
-    def set(self,key,value, expire_time = 24 * 60 * 60 * 1000):
+
+    def set(self, key, value, expire_time=24 * 60 * 60 * 1000):
         cache = self.load()
         cache[key] = {
             'expires': Cache.get_time() + expire_time,
@@ -42,21 +42,54 @@ class Cache:
         self.save(cache)
         print(f'Updated Cache: {key}')
 
-    @staticmethod        
+    @staticmethod
     def get_time():
         return int(round(time.time() * 1000))
 
+
 class GithubAPI:
     def __init__(self):
-        if os.path.exists(os.path.join('.','github_token')):
-            with open(os.path.join('.','github_token')) as file:
+        if os.path.exists(os.path.join('.', 'github_token')):
+            with open(os.path.join('.', 'github_token')) as file:
                 self.token = file.readline()
         else:
             self.token = os.environ['API_GITHUB']
-    
-    def getRequest(self,url: str,headers: dict,params: dict):
+
+        self.cache = Cache('api','github')
+
+    def getRequest(self, url: str, headers: dict={}, params: dict={}):
         headers = headers.copy()
         headers['authorization'] = f'token {self.token}'
+        request = requests.get(url,params=params,headers=headers)
+        if request.status_code == 200:
+            return request
+        else:
+            return None
+    
+    def getAPI(self,url: str,headers: dict={},params: dict={}):
+        key = f'{url} {json.dumps(headers)} {json.dumps(params)}'
+        cached = self.cache.get(key)
+        if cached:
+            return cached
+        data = self.getRequest(url,headers=headers,params=params).json()
+        self.cache.set(key,data)
+        return data
+    
+    def getAPIList(self,url: str,headers: dict={}, params: dict={}, count: int = -1):
+        data = []
+        headers = headers.copy()
+        params = params.copy()
+        params['per_page'] = 100
+        params['page'] = 1
+        while count == -1 or len(data) < count:
+            fetched: list = self.getAPI(url,headers,params)
+            length = len(fetched)
+            while len(fetched) > 0 and len(data) < count:
+                data.append(fetched.pop(0))
+            if length < params['per_page']:
+                break
+            params['page'] = params['page'] + 1
+        return data
 
 
 Github = GithubAPI()
--- python/precompile.py
+++ python/precompile.py
@@ -1,2 +1,4 @@
 from libraries import *
+import json
 
+Github.getAPIList('https://api.github.com/repos/LittleTealeaf/paceManager/commits')
