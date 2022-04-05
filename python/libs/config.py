7f65adfb5706f004b926e995ed6b57ca4b235aa0
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-05 14:51:04 -0400

    Created config.py

---
+++ python/libs/config.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+
+def getPath(*path):
+    return os.path.join('.','config',*path)
+
+def getJSON(*path):
+    data = {}
+    with open(getPath(*path)) as file:
+        data = json.load(file)
+    return data
+
+def getFiles(*path):
+    return os.listdir(getPath(*path))
--- python/precompile.py
+++ python/precompile.py
@@ -1,10 +1,14 @@
 import libs.github as Github
 import libs.generated as gen
+import libs.config as conf
 import json
 
 gen.initialize()
 
 index = {}
+
+index['projects'] = conf.getFiles('projects')
+index['blogs'] = conf.getFiles('blogs')
 
 
 gen.refjson(index,'index.json')
