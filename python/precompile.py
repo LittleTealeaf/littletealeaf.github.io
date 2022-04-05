aceb2c2c68b828b3b919bb5036e730c349efe8b9
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-05 13:31:47 -0400

    Double the pulls

--- python/precompile.py
+++ python/precompile.py
@@ -1,4 +1,5 @@
 from libraries import *
 import json
 
+Github.getAPIList('https://api.github.com/repos/LittleTealeaf/littletealeaf.github.io/commits')
 Github.getAPIList('https://api.github.com/repos/LittleTealeaf/paceManager/commits')
