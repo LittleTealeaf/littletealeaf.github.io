e7c58d74d5f9ce67dac781070695386767d5e493
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-07 17:52:35 -0400

    Cache is split up between files

--- python/libs/cachelib.py
+++ python/libs/cachelib.py
@@ -1,52 +1,53 @@
 import os
 import json
+from random import Random
 import time
 
 
 EXPIRES_DEFAULT = 1000 * 60 * 60 * 24
 
-class Cache:
-    def __init__(self, *path):
-        self.path = os.path.join('.', 'cache', *path) + '.json'
-        for key in self.load():
-            self.get(key)
+VALID_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789_-'
 
-    def load(self):
-        cache = {}
-        if os.path.exists(self.path):
-            with open(self.path) as file:
-                cache = json.load(file)
-        return cache
-
-    def save(self, cache):
-        os.makedirs(os.path.dirname(self.path), exist_ok=True)
-        with open(self.path, 'w') as file:
-            file.write(json.dumps(cache,separators=(',', ':')))
-
-    def get(self, key):
-        cache = self.load()
-        if key in cache:
-            if cache[key]['expires'] > get_time():
-                return cache[key]['value']
-            else:
-                cache.pop(key)
-                self.save(cache)
-        return None
-
-    def set(self, key, value, expires= EXPIRES_DEFAULT):
-        cache = self.load()
-        cache[key] = {
-            'expires': get_time() + expires,
-            'value': value
-        }
-        self.save(cache)
-
-    def analytics(self):
-        cache = self.load()
-        return {
-            'count': len(cache)
-        }
-
+def sanitize_key(key):
+    rand = Random(key)
+    def sanitize(char):
+        if char in VALID_CHARACTERS:
+            return char
+        else:
+            return rand.choice(VALID_CHARACTERS)
+    return ''.join([sanitize(c) for c in key])
 
 def get_time():
     return int(round(time.time() * 1000))
+
+class Cache:
+    def __init__(self,*path):
+        self.path = os.path.join('.','cache',*path)
+
+    def get(self,key):
+        path = self.file_name(key)
+        if os.path.exists(path):
+            with open(path) as file:
+                data = json.load(file)
+                if data['expires'] > get_time():
+                    return data['value']
+        return None
+            
+
+    def set(self,key,value,expires=EXPIRES_DEFAULT):
+        path = self.file_name(key)
+        os.makedirs(os.path.dirname(path),exist_ok=True)
+        with open(path,'w') as file:
+            file.write(json.dumps({
+                'expires': get_time() + expires,
+                'value': value
+            }))
+    
+    def remove(self,key):
+        os.remove(self.file_name(key))
+    
+    def file_name(self,key):
+        return os.path.join(self.path,f'{sanitize_key(key)}.json')
+    
+    def analytics(self):
+        return {}
