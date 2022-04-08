a5409fc13a11e0a27473152177113aa4398ffb7e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-08 10:45:29 -0400

    Clean for more spaced durations

--- python/clean.py
+++ python/clean.py
@@ -15,20 +15,20 @@ def GB(size):
     return 1000 * MB(size)
 
 
-def cleanDir(directory, max_bytes=GB(1)):
+def cleanDir(directory, max_bytes=GB(1), expire_interval=100):
     size = 0
     for dir, _, files in os.walk(directory):
         for f in files:
             fp = os.path.join(dir, f)
             size += os.path.getsize(fp)
     print(directory, size, max_bytes)
-    if size > max_bytes or ('RUN_NUMBER' in os.environ and int(os.environ['RUN_NUMBER']) % 100 == 0):
+    if size > max_bytes or ('RUN_NUMBER' in os.environ and expire_interval != -1 and int(os.environ['RUN_NUMBER']) % expire_interval == 0):
         shutil.rmtree(directory)
         os.makedirs(directory, exist_ok=True)
 
 
 cache.clean()
 
-cleanDir(os.path.join('.', 'cache'), GB(2))
-cleanDir(os.path.join('.', '.next', 'cache'))
-cleanDir(os.path.join('.', 'node_modules'))
+cleanDir(os.path.join('.', 'cache'), GB(2),expire_interval=-1)
+cleanDir(os.path.join('.', '.next', 'cache'),expire_interval=100)
+cleanDir(os.path.join('.', 'node_modules'),expire_interval=50)
