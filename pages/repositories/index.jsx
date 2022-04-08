9e7deb5bc166517b6bda859c0893ad6dda30e291
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-08 11:54:59 -0400

    List repos in repolist

--- pages/repositories/index.jsx
+++ pages/repositories/index.jsx
@@ -1,7 +1,27 @@
+import { getGenerated, index } from "../../libs/resources";
+
 export default function Content({}) {
   return (
     <>
       <div>REPOS</div>
+      <div className="flex flex-wrap" style={{}}>
+        {Object.keys(index.pages.repositories).map((id) => {
+          const repo = getGenerated(index.pages.repositories[id]);
+
+          return (
+            <>
+              <div
+                className="flex-grow-[1]"
+                style={{
+                  padding: "10px",
+                }}
+              >
+                <h1>{repo.api.name}</h1>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </>
   );
 }
