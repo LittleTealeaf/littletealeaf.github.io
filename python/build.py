b20d9c34b709c297a840415097878e12757de29c
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-10 23:13:54 -0400

    Don't expire rate limits

--- python/build.py
+++ python/build.py
@@ -85,7 +85,7 @@ index['analytics'] = Gen('analytics').ref_json({
     'github': {
         'api': Github.getAnalytics(),
         'cache': Github.getAPI('https://api.github.com/repos/LittleTealeaf/littletealeaf.github.io/actions/cache/usage'),
-        'rate_limits': Github.getAPI('https://api.github.com/rate_limit')
+        'rate_limits': Github.getAPI('https://api.github.com/rate_limit',expires=-1,expires_max=-1,expires_min=-1)
     }
 })
 
