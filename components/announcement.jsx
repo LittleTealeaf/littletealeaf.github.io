ba2d759294b55ca1b076e7d9984be45c5c58bd0d
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-12 11:44:30 -0400

    render announcements dynamically

---
+++ components/announcement.jsx
@@ -0,0 +1,25 @@
+import { getGenerated, index } from "../libs/resources";
+
+export default function Announcement({}) {
+  const announcements = getGenerated(index.announcements);
+
+  return (
+    <>
+      <center>
+        {announcements.map((item, index) => (
+          <div
+            key={index}
+            style={{
+              borderRadius: "100px",
+              padding: "10px",
+              width: "70%",
+              background: "pink",
+            }}
+          >
+            {item}
+          </div>
+        ))}
+      </center>
+    </>
+  );
+}
