b9509a50a272024545f722d9aa040f50fd4e5c88
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-13 13:19:19 -0400

    Use name for key

--- components/meta.jsx
+++ components/meta.jsx
@@ -4,14 +4,14 @@ export default function Meta({ values, name, content }) {
   if (values == null) {
     return (
       <Head>
-        <meta name={name} content={content} />
+        <meta key={name} name={name} content={content} />
       </Head>
     );
   } else {
     return (
       <Head>
-        {Object.keys(values).map((name, index) => (
-          <meta key={index} name={name} content={values[name]} />
+        {Object.keys(values).map((name) => (
+          <meta key={name} name={name} content={values[name]} />
         ))}
       </Head>
     );
