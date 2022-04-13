254194bf0a1a771b8ee2957952ecc20f6a1f2239
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-13 08:57:19 -0400

    Added interests

--- config/markdown/aboutme.md
+++ config/markdown/aboutme.md
@@ -1,3 +1,1 @@
-# About Me
-
 Hello, I am a programmer from Connecticut. I like things and I don't know what else I need to type here to be effective
--- pages/about.jsx
+++ pages/about.jsx
@@ -4,6 +4,7 @@ export default function Page({ }) {
     return (
         <>
         <Snippet name='aboutme' />
+        <Snippet name='interests' />
         </>
     );
 }
