0c196a0b8fd3f4b45cc6fdf2b9643303fe705c9c
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-13 13:15:31 -0400

    Fix meta json

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,12 @@
 import { Snippet } from "../components/markdown";
+import Meta from "../components/meta";
 
 export default function Page({}) {
   return (
     <>
+      <Meta values={{
+        page: 'about'
+      }} />
       <Snippet name="aboutme" />
       <Snippet name="interests" />
     </>
