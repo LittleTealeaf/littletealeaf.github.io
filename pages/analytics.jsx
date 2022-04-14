cd96506c9a09a1066cdf4302559729801fa07f81
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-14 12:22:16 -0400

    Analytics now renders proper markdown

--- pages/analytics.jsx
+++ pages/analytics.jsx
@@ -1,14 +1,10 @@
-import { getGenerated, index } from "libs/resources";
-import { RenderMarkdown } from "components/markdown";
+import {Snippet} from 'components/markdown'
 
 export default function Page({}) {
-  const analytics = getGenerated(index.analytics);
-  const json_string =
-    "```json\n" + JSON.stringify(analytics, undefined, 2) + "\n```";
 
   return (
     <>
-      <RenderMarkdown content={json_string} />
+    <Snippet name={'analytics'} />
     </>
   );
 }
