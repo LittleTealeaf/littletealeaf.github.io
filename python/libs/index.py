36fd721e85bc7dd7124da901689cf08ccc5a9070
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-16 14:43:25 -0400

    Created index.py placeholder

---
+++ python/libs/index.py
@@ -0,0 +1,5 @@
+# This is a temporary file that will eventually be turned into an "index" library instead of just doing it all in build.py
+# However, this requires that a temporary location be made
+
+def set(path,value):
+    ...
