672503bfe61d93a1e9a1063093a994adc068dc99
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-17 12:25:57 -0400

    Announcements are now displayed

--- components/announcement.jsx
+++ components/announcement.jsx
@@ -1,26 +1,23 @@
 import { getGenerated, Index } from "libs/resources";
+import {MarkdownAsset} from 'components/markdown';
 
 export default function Announcement({}) {
 
 
   return (
     <center>
-      {/* {announcements.map((item, index) => (
-        <div
-          key={index}
-          style={{
-            borderRadius: "100px",
-            padding: "10px",
-            width: "70%",
-            background: "pink",
-          }}
-        >
-          {item}
+      {Object.keys(Index.markdown.announcements).map((key,index) => (
+        <div key={index}
+        style={{
+          borderRadius: "100px",
+          padding: "10px",
+          width: "70%",
+          background: "pink",
+        }}
+      >
+        <MarkdownAsset asset={Index.markdown.announcements[key]} />
         </div>
-      ))} */}
-      {/* {Object.keys(index.markdown.announcements).map((key) => (
-
-      ))} */}
+      ))}
     </center>
   );
 }
