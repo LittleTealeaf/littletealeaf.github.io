fc6549e9fe80f6a030fd0387d8b703ca55788995
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-18 07:53:48 -0400

    Formatted markdown.jsx

--- components/markdown.jsx
+++ components/markdown.jsx
@@ -1,9 +1,11 @@
 import { getGenerated, Index } from "libs/resources";
 const html_parser = require("html-react-parser");
-import Meta from 'components/meta'
+import Meta from "components/meta";
 
 export function RenderMarkdown({ content }) {
-  return <article className="markdown-body">{html_parser.default(content)}</article>;
+  return (
+    <article className="markdown-body">{html_parser.default(content)}</article>
+  );
 }
 
 /**
@@ -13,14 +15,16 @@ export function Snippet({ name }) {
   return <RenderMarkdown content={getGenerated(Index.snippets[name])} />;
 }
 
-export function MarkdownAsset({asset}) {
+export function MarkdownAsset({ asset }) {
   const item = getGenerated(asset);
-  return <>
-  {
-    Object.keys(item).filter(key => key != 'content').map(key => (
-      <Meta key={key} name={key} content={item[key]} />
-    ))
-  }
-  <RenderMarkdown content={item['content']} />
-  </>
+  return (
+    <>
+      {Object.keys(item)
+        .filter((key) => key != "content")
+        .map((key) => (
+          <Meta key={key} name={key} content={item[key]} />
+        ))}
+      <RenderMarkdown content={item["content"]} />
+    </>
+  );``
 }
