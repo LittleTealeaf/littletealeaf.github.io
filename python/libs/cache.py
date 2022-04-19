071665b7dda690280dad46b6d9f69981340073fd
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-19 15:06:50 -0400

    empty arrays never increment cache time

--- python/libs/cache.py
+++ python/libs/cache.py
@@ -38,7 +38,7 @@ class Cache:
         src = self.load_source(source)
         try:
             if  key in src or duration==-1:
-                if value != None and 'value' in src[key] and src[key]['value'] == value:
+                if value != [] and value != None and 'value' in src[key] and src[key]['value'] == value:
                     duration = min(duration_max,src[key]['duration'] * duration_scale_up)
                 else:
                     duration = max(duration_min,src[key]['duration'] * duration_scale_down)
