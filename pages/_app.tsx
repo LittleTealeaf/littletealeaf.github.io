cd747ad55e9a7b8f67b3f0d2a4a3e261af39e543
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-24 11:31:44 -0400

    Dynamic Imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "libs/components/Header";
 
+/*
+https://nextjs.org/docs/advanced-features/dynamic-import
+*/
+
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Header />
