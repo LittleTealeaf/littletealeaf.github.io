f1f804ee512acb46c5083c9810d2945535e42079
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-26 13:36:05 -0400

    Stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Spacer from "components/spacer";
 import Home from "components/index/home";
 import AboutMe from "components/index/aboutme";
 import Projects from "components/index/projects";
-import GitHub from 'libs/github';
+import API from 'libs/github';
 import css from "styles/pages/index.module.scss";
 
 
@@ -33,8 +33,6 @@ const Content = ({ }) => {
 };
 
 export const getStaticProps = async ({}) => {
-  
-
   return {
 
 
