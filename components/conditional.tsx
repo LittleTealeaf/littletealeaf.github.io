16ff9b54afb465f0784a6e69adb16b93ae901c9a
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-07-08 14:04:58 -0400

    back to html

--- components/conditional.tsx
+++
@@ -1,5 +0,0 @@
-export const RenderIf = (condition: boolean, content: () => JSX.Element) => (condition ? content() : <></>);
-
-export const RenderNotNull = (value: any, content: () => JSX.Element) => RenderIf(value != null, content);
-
-export const RenderNull = (value: any, content: () => JSX.Element) => RenderIf(value == null, content);
--- libs/utils.ts
+++
@@ -1,3 +0,0 @@
-export const filterUnique = <T>(value: T, index: number, self: Array<T>): boolean => self.indexOf(value) === index;
-
-export const BackgroundImage = (url: string, attributes: string = ""): string => `url(${url}) ${attributes}`
