f6750a3bb59d333eff1991585332092a553d0cf7
LittleTealeaf <thomaskwashnak@gmail.com>
2022-07-10 15:37:32 -0400

    So apparently dotenv is being annoying

--- python/github.py
+++ python/github.py
@@ -1,15 +1,20 @@
 import os
-from dotenv import load_dotenv
+
 import requests
 
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    ...
 
 github_token = os.environ.get("API_GITHUB")
 
 
 def getRequest(url: str, headers: dict = {}, params: dict = {}):
     headers = headers.copy()
-    headers['authorization'] = f'token {github_token}'
+    if github_token != None:
+        headers['authorization'] = f'token {github_token}'
     request = requests.get(url, params=params, headers=headers)
     if request.status_code == 200:
         return request
