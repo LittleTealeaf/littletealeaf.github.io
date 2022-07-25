303808c24beee32f34e84f1ccdf28b688bb844c7
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-07-25 16:04:24 -0400

    Updates

--- python/build.py
+++ python/build.py
@@ -1,10 +1,16 @@
 from github import *
 from export import *
 from res import *
+from pathlib import Path
 
 clear_export()
 
-export_image('./resources/images/backgrounds/home.jpg','images','backgrounds','home.webp')
+# export_image('./resources/images/backgrounds/home.jpg','images','backgrounds','home.webp')
+
+for file in os.listdir(get_res('images','background')):
+
+    name = Path(file).stem
+    export_image(get_res('images','background',file),'images','background',f'{name}.webp')
 
 
 # Projects
