692a64f46efbcd14592718e1e0b2edf420913d83
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-08-29 08:38:17 -0400

    Updated index and added favicon

--- pages/index.js
+++ pages/index.js
@@ -5,4 +5,21 @@ fetch("./resources/data/stats.json")
   .then((response) => response.json())
   .then((stats) => {
     const element = document.getElementById("stats");
+
+    const TIMEFRAMES = [
+      {
+        name: "Past 7 Days",
+        data: stats.waka.week,
+      },
+      {
+        name: "Past 30 Days",
+        data: stats.waka.month,
+      },
+      {
+        name: "All Time",
+        data: stats.waka.all,
+      },
+    ];
+
+    
   });
