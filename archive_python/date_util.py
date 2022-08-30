b9bc68ad6c29185957d66d7814a93039c4aea146
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-08-30 07:44:16 -0400

    Delete archives

--- archive_python/date_util.py
+++
@@ -1,8 +0,0 @@
-from datetime import date, datetime
-from dateutil import relativedelta
-
-def time_since_birthday():
-    delta = relativedelta.relativedelta(
-        date.today(), datetime.strptime("05/28/2002", "%m/%d/%Y")
-    )
-    return {"y": delta.years, "m": delta.months, "d": delta.days}
--- archive_python/res.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def get_res(*path: str) -> str:
-    return '/'.join(['.','resources',*path])
-
-def get_json_res(*path: str) -> str:
-    with open(get_res(*path)) as file:
-        return json.load(file)
