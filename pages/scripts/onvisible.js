4e780e2d8c4ac2bf73a178362a77b79d12b67d6b
LittleTealeaf <thomaskwashnak@gmail.com>
2022-09-20 08:43:42 -0400

    Reformatted

--- pages/scripts/onvisible.js
+++ pages/scripts/onvisible.js
@@ -1,14 +1,14 @@
 const elements = document.querySelectorAll(".onvisible");
 
 const observer = new IntersectionObserver((entries) => {
-   entries.forEach((entry) => {
-    if(entry.isIntersecting) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
       entry.target.classList.add("visible");
       entry.target.classList.add("rendered");
     } else {
       entry.target.classList.remove("visible");
     }
-   })
+  });
 });
 
 elements.forEach((element) => observer.observe(element));
