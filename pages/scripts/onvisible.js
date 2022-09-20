b68b33adac3aaf8eb54bdae9d3a5a440e33698c5
LittleTealeaf <thomaskwashnak@gmail.com>
2022-09-20 08:51:41 -0400

    Bit of exploration

--- pages/scripts/onvisible.js
+++ pages/scripts/onvisible.js
@@ -1,14 +1,16 @@
-const elements = document.querySelectorAll(".onvisible");
+(() => {
+  const elements = document.querySelectorAll(".onvisible");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-      entry.target.classList.add("rendered");
-    } else {
-      entry.target.classList.remove("visible");
-    }
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        entry.target.classList.add("rendered");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
   });
-});
 
-elements.forEach((element) => observer.observe(element));
+  elements.forEach((element) => observer.observe(element));
+})();
