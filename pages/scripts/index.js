b70748e68f2ea1cec2f9f8b06bf9eec642c5e862
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-09-21 16:18:20 -0400

    Restart part 2

--- pages/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const BODY = document.querySelector("body");
-
-document.getElementById("drawer_button").addEventListener("click",(_) => {
-  BODY.dataset.drawer = "visible";
-});
-
-document.getElementById("drawer_close").addEventListener("click",(_) => {
-  BODY.dataset.drawer = "hidden";
-});
