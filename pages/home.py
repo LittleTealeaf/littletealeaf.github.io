6a0dbd14155118df0cd5c1f740dc42859caa13a3
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-09-28 10:32:15 -0400

    Moved aboutme.jpg

--- pages/home.py
+++ pages/home.py
@@ -3,12 +3,16 @@ from libs import *
 # TODO build home page
 
 def build():
+
+  github = getGithubApi("https://www.api.github.com/users/LittleTealeaf")
+  print(github['avatar_url'])
+
   return page("Home",{
     "classList": ["_home"],
     "children": [
       {
-        "tag": "h1",
-        "text": "hello world"
+        "tag": "img",
+        "src": export_any_image(github['avatar_url']),
       }
     ]
   })
