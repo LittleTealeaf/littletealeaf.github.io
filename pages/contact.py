67b2e72a714d320b2e4b8010f07bc5b1d88069b8
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-10-04 16:00:13 -0400

    Added baisc contact information

--- pages/contact.py
+++ pages/contact.py
@@ -2,9 +2,32 @@ from libs import *
 
 # TODO build contact page
 
+
 def build():
-  return page("Contact Me",{
-    "classList": ["_contact"],
-    "children": [
-    ],
-  },id="contact")
+    return page(
+        "Contact Me",
+        {
+            "classList": ["_contact"],
+            "children": [
+                {"tag": "h1", "text": "Contact Me!"},
+                {
+                    "classList": ["_contact_options"],
+                    "children": [
+                        {
+                            "component": "button",
+                            "href": "mailto:thomaskwashnak@gmail.com",
+                            "target": "_blank",
+                            "text": "Email Me",
+                        },
+                        {
+                            "component": "button",
+                            "href": "https://www.linkedin.com/in/thomas-kwashnak",
+                            "target": "_blank",
+                            "text": "LinkedIn",
+                        },
+                    ],
+                },
+            ],
+        },
+        id="contact",
+    )
