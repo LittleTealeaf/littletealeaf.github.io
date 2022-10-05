03559455d8b2333c067d3442ac534b97a9bd1999
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-10-05 18:30:14 -0400

    Add info about site being in progress
    Currently prepping to take over main perhaps?

--- pages/home.py
+++ pages/home.py
@@ -20,7 +20,10 @@ def build():
                 {
                     "tag": "h3",
                     "text": "Feel free to explore with the drawer on the left!",
-                },
+                },{
+                    "tag": "h3",
+                    "text": "Please keep in mind that this site may be a work in progress!"
+                }
             ],
         },
         id="home",
