f89ab4f2258553ece49e38f8e31dbc28cb203a8d
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-10-10 10:54:09 -0400

    Added button component to dom.py

--- libs/dom.py
+++ libs/dom.py
@@ -2,46 +2,45 @@
 
 
 def node(
-  tag: str = "div",
-  amp: str = None,
-  classList: list[str] = None,
-  children: list[dict] = None,
-  href: str = None,
-  text: str = None,
-  src: str = None,
-  alt: str = None,
-  target: str = None,
-  style: dict = None,
-  attributes: dict = None,
-  html: str = None,
-  other: dict = None
+    tag: str = "div",
+    amp: str = None,
+    classList: list[str] = None,
+    children: list[dict] = None,
+    href: str = None,
+    text: str = None,
+    src: str = None,
+    alt: str = None,
+    target: str = None,
+    style: dict = None,
+    attributes: dict = None,
+    html: str = None,
+    other: dict = None,
 ):
-  element = {
-    'tag': tag,
-    'amp': amp,
-    'classList': classList,
-    'children': children,
-    'href': href,
-    'text': text,
-    'src': src,
-    'alt': alt,
-    'target': target,
-    'style': style,
-    'attributes': attributes,
-    'html': html
-  }
+    element = {
+        "tag": tag,
+        "amp": amp,
+        "classList": classList,
+        "children": children,
+        "href": href,
+        "text": text,
+        "src": src,
+        "alt": alt,
+        "target": target,
+        "style": style,
+        "attributes": attributes,
+        "html": html,
+    }
 
-  if other:
-    for key,value in other:
-      element[key] = value
+    if other:
+        for key, value in other:
+            element[key] = value
 
-  return {
-    key: value for key, value in element.items() if value is not None
-  }
+    return {key: value for key, value in element.items() if value is not None}
+
 
 def details(title: str = None, content: str = None):
-  return {
-    'component': 'details',
-    'title': title,
-    'content': content
-  }
+    return {"component": "details", "title": title, "content": content}
+
+
+def button(text: str = None, href: str = None, target: str = None):
+    return node(text=text, href=href, target=target, other={"component": "button"})
