d7787bf98c99de351f8f124080fb8ba97d9caa85
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-05-27 15:46:30 -0400

    Removed items

--- pages/__init__.py
+++
@@ -1,6 +0,0 @@
-import pages.home as home
-import pages.projects as projects
-import pages.about_me as about_me
-import pages.contact as contact
-import pages.resume as resume
-import pages.stats as stats
--- pages/resume.py
+++
@@ -1,9 +0,0 @@
-from libs import *
-
-
-# TODO build resume page
-
-
-def build():
-    with open("./resources/resume.json") as file:
-        return page("Resume", json.load(file), "resume", id="resume")
