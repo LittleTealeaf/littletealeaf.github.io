0897df488c124e23e746e27290948b6b15fe0554
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-05-29 18:49:45 -0400

    Removed material icons (as nerd fonts includes those)

--- python/wakatime.py
+++ python/wakatime.py
@@ -1,3 +1,17 @@
-from requests import request
+import requests
+import os
+import shutil
 
+
+export_path = os.path.join("src", "data", "wakatime")
+if os.path.exists(export_path):
+    shutil.rmtree(export_path)
+
+
+
+# EMBEDDABLE JSONS
+response = requests.get("https://wakatime.com/share/@LittleTealeaf/a74583c7-30a4-4922-95e0-08c97a8d79cb.json")
+year_activity = response.json()
+assert 'days' in year_activity
+year_activity = year_activity['days']
 
