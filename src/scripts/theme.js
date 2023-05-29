5d166ea94e4cb2eabb48ea27b750b871aec22234
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-05-29 17:20:20 -0400

    Moved some calls to onload

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,38 +1,31 @@
 function setTheme(theme) {
-	const FLAVORS = ["latte", "frappe", "macchiato", "mocha"];
-	if (!FLAVORS.includes(theme)) {
-		console.error(
-			"Invalid Theme, Expected one of [latte, frappe, macchiato, mocha]"
-		);
-		return;
-	}
+  const FLAVORS = ["latte", "frappe", "macchiato", "mocha"];
+  if (!FLAVORS.includes(theme)) {
+    console.error("Invalid Theme, Expected one of [latte, frappe, macchiato, mocha]");
+    return;
+  }
 
-	document.body.dataset.theme = theme;
+  document.body.dataset.theme = theme;
 
-	document.querySelectorAll(`[data-theme-option]`).forEach((element) => {
-		element.dataset.selected = element.dataset.themeOption == theme;
-	});
+  document.querySelectorAll(`[data-theme-option]`).forEach((element) => {
+    element.dataset.selected = element.dataset.themeOption == theme;
+  });
 }
 
 {
-	const load_function = window.onload || function() { };
+  const load_function = window.onload || function () {};
 
-	window.onload = function() {
-		load_function();
+  window.onload = function () {
+    load_function();
 
-		if (
-			window.matchMedia &&
-			window.matchMedia("(prefers-color-scheme: light)").matches
-		) {
-			setTheme("latte");
-		} else {
-			setTheme("macchiato");
-		}
-	};
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) {
+      setTheme("latte");
+    } else {
+      setTheme("macchiato");
+    }
 
-	window
-		.matchMedia("(prefers-color-scheme: dark)")
-		.addEventListener("change", (event) => {
-			setTheme(event.matches ? "macchiato" : "latte");
-		});
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+      setTheme(event.matches ? "macchiato" : "latte");
+    });
+  };
 }
