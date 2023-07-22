95f274920084fea62fca6dbac0f3b2915d318143
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-07-22 15:58:10 -0400

    fun stuff

--- python/wakatime.py
+++ python/wakatime.py
@@ -1,17 +1,35 @@
 import requests
 import os
+import json
 import shutil
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 export_path = os.path.join("src", "data", "wakatime")
 if os.path.exists(export_path):
     shutil.rmtree(export_path)
 os.makedirs(os.path.join("src", "data"), exist_ok=True)
+os.makedirs(os.path.join("src","data","wakatime"), exist_ok=True)
 
+params = {
+    'api_key': os.getenv("WAKA_TOKEN")
+}
 
 # EMBEDDABLE JSONS
-response = requests.get("https://wakatime.com/share/@LittleTealeaf/a74583c7-30a4-4922-95e0-08c97a8d79cb.json")
-year_activity = response.json()
-assert 'days' in year_activity
-year_activity = year_activity['days']
+all_time_stats = requests.get(
+    "https://wakatime.com/api/v1/users/current/stats/all_time", params=params
+).json()
 
+for project in all_time_stats['data']['projects']:
+    if project['name'] == 'dotfiles':
+        dotfiles_data = {
+            'hours': project['hours'],
+            'minutes': project['minutes'],
+        }
+        with open(os.path.join("src", "data", "wakatime", "dotfiles_time.json"), 'w') as file:
+            file.write(json.dumps(dotfiles_data))
+
+
+# 
